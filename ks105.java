de716b3c1c5127708d2e7fba9b2ade57bbb46387
sumakannan <46103282+sumakannan@users.noreply.github.com>
2019-01-06 02:57:53 -0800

    Create ks105.java

---
+++ ks105.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.Scanner;
+class ks105
+{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int p=sc.nextInt();
+		int a=10,q;
+		int num;
+		q=p;
+		while(p!=0)
+		{
+		a=a*10;
+			p=p/10;
+		}
+		if(q==0)
+		{
+num=n*a+q;
+}
+else
+{
+	a=a/10;
+	num=n*a+q;
+}
+System.out.println(num);
+	}
+
+}
