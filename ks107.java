9ff485b036f5508094df59970d8bbe6f847cb53a
sumakannan <46103282+sumakannan@users.noreply.github.com>
2019-01-06 03:46:38 -0800

    Create ks107.java

---
+++ ks107.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class ks107
+{
+	public static void main(String[] args)throws IOException
+	{
+Scanner sc=new Scanner(System.in);
+int n,m,p,s;
+n=sc.nextInt();
+m=sc.nextInt();
+p=sc.nextInt();
+s=(n*m)/p;
+ System.out.println(s);
+}
+}
