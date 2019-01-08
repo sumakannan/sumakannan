f28387c6d023ad770ef4f155271a8cdc3a25fcee
sumakannan <46103282+sumakannan@users.noreply.github.com>
2019-01-08 06:47:53 -0800

    Create ks19.java

---
+++ ks19.java
@@ -0,0 +1,24 @@
+
+
+import java.util.Scanner;
+import java.lang.*;
+import java.io.*;
+
+class ks19
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int i,s=1;
+		for(i=n;i>0;i--)
+		{
+			s=s*i;
+			
+			
+			
+		}
+		System.out.println(s);
+		
+	}
+}
