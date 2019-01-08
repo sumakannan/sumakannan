2d3e6914008efa124bbea152702aae465f0f3714
sumakannan <46103282+sumakannan@users.noreply.github.com>
2019-01-08 06:21:48 -0800

    Create ks20.java

---
+++ ks20.java
@@ -0,0 +1,22 @@
+
+
+import java.util.Scanner;
+import java.lang.*;
+import java.io.*;
+
+class ks20
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int i;
+		for(i=1;i<=5;i++)
+		{
+			System.out.print(n*i);
+			
+			
+		}
+		
+	}
+}
