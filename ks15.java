358cf76b11f55e11c7c4365f57057d8538cce11c
sumakannan <46103282+sumakannan@users.noreply.github.com>
2019-01-08 06:57:48 -0800

    Create ks15.java

---
+++ ks15.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.lang.*;
+import java.io.*;
+
+class ks15
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		int i;
+          for(i=n1+1;i<n2;i+=2)
+          {
+                if(i%2==0)
+                    {
+                      System.out.println(i);
+     
+                    }
+}
+		
+	}
+}
