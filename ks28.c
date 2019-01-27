ed91e50ae4c3c598b9db6c434cc938f41a74fe5b
sumakannan <46103282+sumakannan@users.noreply.github.com>
2019-01-27 10:40:40 +0530

    Create ks28.c

---
+++ ks28.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int a[30],i,n;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d %d",a[i],i);
+		printf("\n");
+	}
+	return 0;
+}
