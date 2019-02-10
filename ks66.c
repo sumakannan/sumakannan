6d8fbfcef0fcb6535eeedb00c44655e9226c095b
sumakannan <46103282+sumakannan@users.noreply.github.com>
2019-02-10 09:34:10 +0530

    Create ks66.c

---
+++ ks66.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,i,flag=0;
+	scanf("%d",&n);
+	for(i=2;i<=n/2;i++)
+	{
+		if(n%i==0)
+		{
+		flag=1;
+		break;
+		}
+	}
+	if(flag==1)
+	printf("no");
+	else
+	printf("yes");
+	return 0;
+}
